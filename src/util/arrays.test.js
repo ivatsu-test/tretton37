f7be2a2e3417d34340740c8c8c1051433df1a03a
IvaTsu <ivan.tsud@gmail.com>
2022-07-03 12:48:06 +0200

    feat: create sort utilities and cover with tests

---
+++ src/util/arrays.test.js
@@ -0,0 +1,225 @@
+import { sortByEmployeeName, sortByEmployeeOffice, SortOrder } from './arrays';
+
+// sortByEmployeeName
+
+const notSortedEmployees = [
+  {
+    name: 'Martin Nilsson',
+  },
+  {
+    name: 'Johannes Alvarsson',
+  },
+  {
+    name: 'Ludwig Schönbeck',
+  },
+  {
+    name: 'Helen Toomik',
+  },
+  {
+    name: 'Georgy Sayganov',
+  },
+  {
+    name: 'Kim Gustafsson',
+  },
+  {
+    name: 'Jens Norell',
+  },
+  {
+    name: 'Håkan Haraldsson',
+  },
+  {
+    name: 'Martin Lecke',
+  },
+];
+
+const sortedEmployeeNamesAsc = [
+  {
+    name: 'Georgy Sayganov',
+  },
+  {
+    name: 'Helen Toomik',
+  },
+  {
+    name: 'Håkan Haraldsson',
+  },
+  {
+    name: 'Jens Norell',
+  },
+  {
+    name: 'Johannes Alvarsson',
+  },
+  {
+    name: 'Kim Gustafsson',
+  },
+  {
+    name: 'Ludwig Schönbeck',
+  },
+  {
+    name: 'Martin Lecke',
+  },
+  {
+    name: 'Martin Nilsson',
+  },
+];
+
+const sortedEmployeeNamesDesc = [
+  {
+    name: 'Martin Nilsson',
+  },
+  {
+    name: 'Martin Lecke',
+  },
+  {
+    name: 'Ludwig Schönbeck',
+  },
+  {
+    name: 'Kim Gustafsson',
+  },
+  {
+    name: 'Johannes Alvarsson',
+  },
+  {
+    name: 'Jens Norell',
+  },
+  {
+    name: 'Håkan Haraldsson',
+  },
+  {
+    name: 'Helen Toomik',
+  },
+  {
+    name: 'Georgy Sayganov',
+  },
+];
+
+it.each([
+  [
+    notSortedEmployees,
+    sortedEmployeeNamesAsc,
+  ],
+])('%p should return ASC order %p', (value, expected) => {
+  const result = sortByEmployeeName(value, SortOrder.ASC);
+  expect(result).toStrictEqual(expected);
+});
+
+it.each([
+  [
+    notSortedEmployees,
+    sortedEmployeeNamesDesc,
+  ],
+])('%p should return ASC order %p', (value, expected) => {
+  const result = sortByEmployeeName(value, SortOrder.DESC);
+  expect(result).toStrictEqual(expected);
+});
+
+// sortByEmployeeOffice
+
+const notSortedEmployeesOffice = [
+  {
+    office: 'Stockholm',
+  },
+  {
+    office: 'Stockholm',
+  },
+  {
+    office: 'Lund',
+  },
+  {
+    office: 'Stockholm',
+  },
+  {
+    office: 'Lund',
+  },
+  {
+    office: 'Lund',
+  },
+  {
+    office: 'Lund',
+  },
+  {
+    office: 'Stockholm',
+  },
+  {
+    office: 'Lund',
+  },
+];
+
+const sortedEmployeeOfficeAsc = [
+  {
+    office: 'Lund',
+  },
+  {
+    office: 'Lund',
+  },
+  {
+    office: 'Lund',
+  },
+  {
+    office: 'Lund',
+  },
+  {
+    office: 'Lund',
+  },
+  {
+    office: 'Stockholm',
+  },
+  {
+    office: 'Stockholm',
+  },
+  {
+    office: 'Stockholm',
+  },
+  {
+    office: 'Stockholm',
+  },
+];
+
+const sortedEmployeeOfficeDesc = [
+  {
+    office: 'Stockholm',
+  },
+  {
+    office: 'Stockholm',
+  },
+  {
+    office: 'Stockholm',
+  },
+  {
+    office: 'Stockholm',
+  },
+  {
+    office: 'Lund',
+  },
+  {
+    office: 'Lund',
+  },
+  {
+    office: 'Lund',
+  },
+  {
+    office: 'Lund',
+  },
+  {
+    office: 'Lund',
+  },
+];
+
+it.each([
+  [
+    notSortedEmployeesOffice,
+    sortedEmployeeOfficeAsc,
+  ],
+])('%p should return ASC order %p', (value, expected) => {
+  const result = sortByEmployeeOffice(value, SortOrder.ASC);
+  expect(result).toStrictEqual(expected);
+});
+
+it.each([
+  [
+    notSortedEmployeesOffice,
+    sortedEmployeeOfficeDesc,
+  ],
+])('%p should return ASC order %p', (value, expected) => {
+  const result = sortByEmployeeOffice(value, SortOrder.DESC);
+  expect(result).toStrictEqual(expected);
+});
